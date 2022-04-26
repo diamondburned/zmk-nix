ae2fb855798fc82340ce4ba8bb7751f6b2105708
diamondburned <diamond@arikawa-hi.me>
2022-04-26 02:13:23 +0000

    Add gnuarmemb

---
+++ config.nix
@@ -0,0 +1,20 @@
+{ pkgs }:
+
+with pkgs.lib;
+
+{
+	keymaps = {
+		"shields/corne" = pkgs.fetchgit {
+			url = "https://github.com/diamondburned/zmk-config-corne";
+			rev = "d3db3cac52b7e1a6f9352ab47e6df73f1158a319";
+			sha256 = "1yi88kbsdl7jpj1yv0rrzkp10cfzv3nnf0v9fjfrhmhrv7vyk8vd";
+		} + "/config/corne.keymap";
+	};
+	build = {
+		board = "nice_nano";
+		shields = [
+			"corne_left"
+			"corne_right"
+		];
+	};
+}
--- shell.nix
+++ shell.nix
@@ -1,24 +1,51 @@
 { pkgs ? import <nixpkgs> {} }:
 
 # https://sourcegraph.com/github.com/qmk/qmk_firmware/-/blob/shell.nix
+# https://github.com/nickcoutsos/keymap-editor
 
-let avrlibc = pkgs.pkgsCross.avr.libcCross;
-	avrincl = pkgs.lib.concatMapStrings (x: x + " ") [
-		"-isystem ${avrlibc}/avr/include"
-		"-B${avrlibc}/avr/lib/avr5"
-		"-L${avrlibc}/avr/lib/avr5"
-		"-B${avrlibc}/avr/lib/avr35"
-		"-L${avrlibc}/avr/lib/avr35"
-		"-B${avrlibc}/avr/lib/avr51"
-		"-L${avrlibc}/avr/lib/avr51"
-	];
+let config = import ./config.nix { inherit pkgs; };
+
+	lib  = pkgs.lib;
+	root = builtins.toString ./.; # absolute path within system
+
+	keymaps = pkgs.writeScript "keymaps.sh" (
+		lib.mapAttrsToList
+			(dst: src: "cp ${src} ./zmk/app/boards/${dst}/")
+			(config.keymaps)
+	);
+
+	build-zmk = pkgs.writeShellScriptBin "build-zmk" ''
+		set -e
+		rm $(find ${root} -name CMakeCache.txt)
+		cd ${root}/zmk/app
+
+		export ZEPHYR_TOOLCHAIN_VARIANT=gnuarmemb
+		# ./zmk/zephyr/cmake/toolchain/gnuarmemb/generic.cmake:19
+		# ./zmk/zephyr/cmake/toolchain/gnuarmemb/generic.cmake:28
+		export GNUARMEMB_TOOLCHAIN_PATH=/usr
+
+		shields=(${lib.concatStringsSep " " config.build.shields})
+		for shield in ''${shields[@]}; {
+			west build -d build/$shield -b ${config.build.board} -- -DSHIELD=$shield
+		}
+
+		echo "Outputs:"
+		printf -- "- ${root}/zmk/app/build/%s\n" "''${shields[@]}"
+	'';
 
 	initFile = pkgs.writeText "init.sh" ''
-		export AVR_CFLAGS="${avrincl}"
-		export AVR_ASFLAGS="${avrincl}"
-
 		virtualenv venv
 		. venv/bin/activate
+
+		[[ -d ${root}/zmk/modules/ ]] || (
+			cd ${root}/zmk
+			west init -l app/
+			west update
+			west zephyr-export
+			pip3 install -r zephyr/scripts/requirements-base.txt
+		)
+
+		. ${keymaps}
 		. ~/.bashrc
 	'';
 
@@ -34,13 +61,13 @@ let avrlibc = pkgs.pkgsCross.avr.libcCross;
 			clang-tools
 			coreutils
 			cmake
+			ninja
 			dfu-programmer
 			dfu-util
-			pkgsCross.avr.buildPackages.binutils
-			pkgsCross.avr.buildPackages.gcc8
-			avrlibc
-			avrdude
+			gcc-arm-embedded-10
 			git
+
+			build-zmk
 		];
 
 		runScript = ''bash --init-file ${initFile}'';
